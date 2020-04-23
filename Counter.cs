01e61d5f42cb367c399172733fbc183f5f164968
Ilya Gryaznev <gryaznev97@gmail.com>
2020-04-23 19:19:07 +0300

    '

---
+++ Counter.cs
@@ -0,0 +1,18 @@
+using System; 
+using System.IO; 
+using System.Text.RegularExpressions; 
+using System.Collections; 
+
+namespace WordCounter 
+{ 
+class Counter 
+{ 
+public static int doSomething(string text) 
+{ 
+string[] arrText = text.Split(' '); 
+
+return arrText.Length; 
+//ии 
+} 
+} 
+}
